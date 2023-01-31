39883fea9b753ef15cd4ddb261550e77868a51bf
ceckert2194 <ceckert2194@gmail.com>
2023-01-30 19:04:17 -0500

    main returns Result

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,15 @@ impl Drop for CleanUp {
     }
 }
 
-fn main() {
+fn main() -> crossterm::Result<()> { // this means main will return a Result {
 
     let _clean_up = CleanUp;
 
-    terminal::enable_raw_mode().expect("Couldn't enable raw mode.");
+    terminal::enable_raw_mode()?; // changed to shorthand for expect (? can only be used in a method that returns a Result or Option)
 
     loop { // looping keeps the program alive and reading lines
-        if event::poll(Duration::from_millis(500)).expect("Error") {
-            if let Event::Key(event) = event::read().expect("Failed to read line.") { // checks if Event returned is a Key -- if program fails to read the line, prints msg
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(event) = event::read()? { // checks if Event returned is a Key -- if program fails to read the line, prints msg
                 match event { // attempts to match event (input) to corresponding enum in crossterm::Event
                     KeyEvent {
                         code: KeyCode::Char('q'), 
@@ -35,28 +35,6 @@ fn main() {
             println!("No input yet.\r");
         }
     }
-
-
-    /* old with std lib
-
-    let mut buffer = [0; 1]; // read one byte at a time
-
-
-    while io::stdin().read(&mut buffer).expect("Failed to read line.") == 1 && buffer != [b'q'] {
-
-        let character = buffer[0] as char;
-        // control characters are non-printable characters (ASCII 0-31 + 127)
-        if character.is_control() {
-            // \r (carriage return) will move the cursor back to the left side of the screen so it doesn't look choppy when typing
-            println!("{}\r", character as u8)
-        } else {
-            println!("{}\r", character)
-        }
-
-    }     
-    // ctrl-D to tell program it's the end of the file or ctrl-C to terminate
-    // the code after the && allows us to enter q to quit -- the prefix b designates a byte literal which is equivalent to a u8 (the type buffer takes
-    */
-
+    Ok(())
 
 }
